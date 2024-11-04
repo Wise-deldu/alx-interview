a3efce17d6a98b4bca0232499faa47e4f1aed516
Wise-deldu <delduwise@gmail.com>
2024-11-04 15:14:44 +0000

    Fixed pycodestyle error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,7 +7,7 @@ Pascal's Triangle
 def pascal_triangle(n):
     """
     returns a list of lists of
-    integers representing the 
+    integers representing the
     Pascal's triangle of n
     Returns an empty list if n <= 0
     """
